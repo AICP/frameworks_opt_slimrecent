110cefea540752d870367d0eb6415e77c550fc7b
ezio84 <brabus84@gmail.com>
2017-10-30 22:27:10 +0100

    Make cards refresh more reliable
    thanks to this smart fix by @SpiritCroc
    http://gerrit.aicp-rom.com/#/c/49510/
    we can now refresh cards with their real time index
    and avoid any wrong app icon or thumbnail reference
    
    Change-Id: Ic2a175f0da252f6b6639c284ac68dfdf3a927d78

--- src/com/android/systemui/slimrecent/RecentPanelView.java
+++ src/com/android/systemui/slimrecent/RecentPanelView.java
@@ -216,7 +216,7 @@ public class RecentPanelView {
             this.refreshListener = new ExpandableCardAdapter.RefreshListener() {
                 @Override
                 public void onRefresh(int index) {
-                    postnotifyItemChanged(mCardRecyclerView, index, null);
+                    postnotifyItemChanged(mCardRecyclerView, getExpandableCard(), false);
                 }
             };
 
@@ -1121,8 +1121,6 @@ public class RecentPanelView {
         private void addCard(final TaskDescription task, boolean topTask) {
             RecentCard card = null;
 
-            final int index = mCounter;
-
             card = new RecentCard(task);
 
             final ExpandableCard ec = card;
@@ -1132,14 +1130,14 @@ public class RecentPanelView {
                     .getBitmapFromMemCache(task.identifier);
             if (appIcon != null) {
                 ec.appIcon = appIcon;
-                postnotifyItemChanged(mCardRecyclerView, index, null);
+                postnotifyItemChanged(mCardRecyclerView, ec, false);
             } else {
                 AppIconLoader.getInstance(mContext).loadAppIcon(task.info,
                         task.identifier, new AppIconLoader.IconCallback() {
                             @Override
                             public void onDrawableLoaded(Drawable drawable) {
                                 ec.appIcon = drawable;
-                                postnotifyItemChanged(mCardRecyclerView, index, null);
+                                postnotifyItemChanged(mCardRecyclerView, ec, false);
                             }
                 }, mScaleFactor);
             }
@@ -1153,7 +1151,7 @@ public class RecentPanelView {
                 if (screenshot != null) {
                     preloadedThumbNum++;
                     ec.screenshot = screenshot;
-                    postnotifyItemChanged(mCardRecyclerView, index, ec);
+                    postnotifyItemChanged(mCardRecyclerView, ec, true);
                 } else {
                     new BitmapDownloaderTask(mContext, mScaleFactor,
                             mThumbnailHeight, mThumbnailWidth, task.identifier,
@@ -1162,7 +1160,7 @@ public class RecentPanelView {
                         public void onBitmapLoaded(Bitmap bitmap) {
                             preloadedThumbNum++;
                             ec.screenshot = bitmap;
-                            postnotifyItemChanged(mCardRecyclerView, index, ec);
+                            postnotifyItemChanged(mCardRecyclerView, ec, true);
                         }
                     }).executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR,
                             task.persistentTaskId);
@@ -1227,18 +1225,18 @@ public class RecentPanelView {
     };
 
     private void postnotifyItemChanged(final RecyclerView recyclerView,
-            int index, ExpandableCard ec) {
+            ExpandableCard ec, boolean bitmapLoading) {
         Handler handler = new Handler(Looper.getMainLooper());
         handler.post(new Runnable() {
             @Override
             public void run() {
                 if (!recyclerView.isComputingLayout()) {
-                    mCardAdapter.notifyItemChanged(index);
-                    if (ec != null) {
+                    mCardAdapter.notifyItemChanged(ec.index);
+                    if (bitmapLoading) {
                         ec.needsThumbLoading = false;
                     }
                 } else {
-                    postnotifyItemChanged(recyclerView, index, ec);
+                    postnotifyItemChanged(recyclerView, ec, bitmapLoading);
                 }
             }
         });
